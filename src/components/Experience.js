23cf458f3a9c87bfc9a7181a83b8b65dd69139e9
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-05 14:12:13 +0700

    Add: Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -16,7 +16,7 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
         whileInView={{ y: 0 }}
         transition={{ duration: 0.5, type: "spring" }}
       >
-        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+        <h3 className="text-2xl font-bold capitalize sm:text-xl xs:text-lg">
           {position}{" "}
           <a
             className="capitalize text-slideGreen dark:text-primaryDark"
@@ -26,10 +26,10 @@ const Details = ({ position, company, companyLink, time, address, work }) => {
             @{company}
           </a>
         </h3>
-        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+        <span className="font-medium capitalize text-dark/75 dark:text-light/50 xs:text-sm">
           {time} | {address}
         </span>
-        <p className="font-medium w-full md:text-sm"> {work}</p>
+        <p className="w-full font-medium text-justify md:text-sm"> {work}</p>
       </motion.div>
     </li>
   );
@@ -45,7 +45,7 @@ const Experience = () => {
 
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+      <h2 className="w-full mb-32 font-bold text-center text-8xl md:text-6xl xs:text-4xl md:mb-16">
         Experience
       </h2>
 
@@ -56,27 +56,36 @@ const Experience = () => {
           style={{ scaleY: scrollYProgress }}
         />
 
-        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+        <ul className="flex flex-col items-start justify-between w-full ml-4 xs:ml-2">
           <Details
-            position="Developer"
-            company="Stellar Innovations"
-            time="2020 - 2024"
-            address="200 Innovations st"
+            position="Business Process Automation Engineer"
+            company="Braincore.id"
+            time="Jan 2024 - Present"
+            address="Jakarta, Indonesia (Freelance)"
             companyLink="https://github.com/lilxyzz/clay-theme"
             work="At Stellar Innovations, I serve as a web developer, where I play a crucial role in designing and implementing cutting-edge web solutions. My responsibilities include developing responsive and high-performance websites and applications using technologies such as HTML, CSS, JavaScript, React, and Node.js. I collaborate closely with cross-functional teams to ensure our projects meet the highest standards of functionality and user experience. One of my key achievements at Stellar Innovations has been leading the development of a comprehensive e-commerce platform that significantly enhanced our client's online presence and sales. My work is driven by a passion for continuous learning and innovation, always striving to push the boundaries of what's possible in web development."
           />
 
           <Details
-            position="Rad Tester"
-            company="Vault Technologies"
-            time="2018 - 2020"
-            address="404 Tech st"
+            position="Mobile Development Cohort"
+            company="Bangkit Academy 2023 Batch 2"
+            time="Aug 2023 - Dec 2023"
+            address="Jakarta, Indonesia (Remote)"
+            companyLink="https://github.com/lilxyzz/holo-theme"
+            work="At Vault Technologies, I worked as a RAD (Rapid Application Development) Tester, where I was responsible for ensuring the quality and functionality of our software applications during the development process. My role involved designing and executing test plans, identifying and documenting bugs, and working closely with developers to resolve issues quickly. This position required a deep understanding of the software development lifecycle and a keen eye for detail to ensure our applications met the highest standards of performance and reliability. My contributions at Vault Technologies helped streamline our development processes and significantly improved the overall quality of our software products."
+          />
+
+          <Details
+            position="Pengembangan Front-End Web dan Back-End"
+            company="Dicoding X Kampus Merdeka Cycle 4"
+            time="Jan 2023 - Jun 2023"
+            address="Jakarta, Indonesia (Remote)"
             companyLink="https://github.com/lilxyzz/holo-theme"
             work="At Vault Technologies, I worked as a RAD (Rapid Application Development) Tester, where I was responsible for ensuring the quality and functionality of our software applications during the development process. My role involved designing and executing test plans, identifying and documenting bugs, and working closely with developers to resolve issues quickly. This position required a deep understanding of the software development lifecycle and a keen eye for detail to ensure our applications met the highest standards of performance and reliability. My contributions at Vault Technologies helped streamline our development processes and significantly improved the overall quality of our software products."
           />
         </ul>
       </div>
-      <div className="mt-40 flex items-center justify-between gap-3 grid-cols-2">
+      <div className="flex items-center justify-between grid-cols-2 gap-3 mt-40">
         <Link
           href="/projects/"
           target={"_self"}
