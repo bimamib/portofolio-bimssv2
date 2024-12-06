c8376b196da148a3955db016678d8bab20935996
ʙ ɪ ᴍ ᴀ ᴀ ᴀ <bimaprast1601@gmail.com>
2024-12-06 21:57:14 +0700

    Add: Button Download CV

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,6 +76,18 @@ export default function Home() {
                 >
                   projects
                 </Link>
+                <Link
+                  download=""
+                  href="/pdf/CV-BimaPrasetio.pdf"
+                  target={"_blank"}
+                  className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+                    capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+                    dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+                    md:p-2 md:px-4 md:text-base
+                    `}
+                >
+                  Download CV
+                </Link>
               </div>
             </div>
           </div>
